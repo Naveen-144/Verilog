78c661f554079af46e914be239f603c69e8e0014
Kumar K <svg.kumaran@gmail.com>
2025-04-18 18:58:08 +0530

    added code

--- four_bit_adder.v
+++ four_bit_adder.v
@@ -1,1 +1,10 @@
-module adder(sum,carry,a,b)
+module four_bit_adder (
+    input  [3:0] A,     // First 4-bit input
+    input  [3:0] B,     // Second 4-bit input
+    output [3:0] SUM,   // 4-bit sum output
+    output       COUT   // Carry out
+);
+
+    assign {COUT, SUM} = A + B;
+
+endmodule
